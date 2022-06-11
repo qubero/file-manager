70089892b1d5521e7c78cbe315b864b9ffe9e249
wolsos-v <mamaev757@gmail.com>
2022-06-11 19:23:28 +0300

    feat: add handling for paths with spaces

--- src/cli/handleInput.js
+++ src/cli/handleInput.js
@@ -1,15 +1,26 @@
 import { FMcommands } from './commands.js';
 import { FMerror, FMmessage } from '../logger/logger.js';
 
-export const handleInput = (input) => {
-  const [command, ...args] = input.split(' ');
+export const handleInput = async (input) => {
+  input = input.trim();
+
+  let [command, ...args] = input.split(' ');
+
+  //TODO: fix input args parse
+  if (/["']/g.test(args)) {
+    args = args
+      .join(' ')
+      .split(/["'] | ["']/)
+      .map((arg) => arg.replace(/["']/g, ''))
+  }
 
   try {
     if (!FMcommands.has(command)) throw FMerror.unknown;
 
-    FMcommands.get(command).action(args);
-    FMmessage.showCwd();
+    await FMcommands.get(command).action(args);
   } catch (err) {
     console.error(err.message);
+  } finally {
+    FMmessage.showCwd();
   }
 };
