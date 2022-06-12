b498ef6d64de0521616c42c241747abe539e7e3f
wolsos-v <mamaev757@gmail.com>
2022-06-12 20:05:24 +0300

    fix: check file exists

--- src/commands/bfo/cp/index.js
+++ src/commands/bfo/cp/index.js
@@ -1,25 +1,33 @@
-import { parse, resolve } from 'path';
+import { basename, resolve } from 'path';
+import { open } from 'fs/promises';
 import { pipeline } from 'stream/promises';
-import { createReadStream, createWriteStream } from 'fs';
 import { FMerror } from '../../../logger/logger.js';
 
 const CP_PARAMS_COUNT = 2;
 
 const copyFile = async (params) => {
+  let fhs = null,
+      fhd = null;
+
   try {
     if (params.length !== CP_PARAMS_COUNT) throw error;
 
     const [fromPath, toPath] = params;
     const absFromPath = resolve(fromPath);
-    const { base } = parse(fromPath);
-    const absToPath = resolve(toPath, base);
+    const absToPath = resolve(toPath, basename(fromPath));
+
+    fhs = await open(absFromPath, 'r');
+    fhd = await open(absToPath, 'wx');
 
     await pipeline(
-      createReadStream(absFromPath),
-      createWriteStream(absToPath)
+      fhs.createReadStream(),
+      fhd.createWriteStream()
     );
   } catch (err) {
     throw FMerror.fail;
+  } finally {
+    await fhs?.close();
+    await fhd?.close();
   }
 };
 
